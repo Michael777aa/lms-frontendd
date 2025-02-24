68391ad332f7f13a3251d6cfd124c77772c7b7e2
Michael777aa <shoxabdufattoyev485@gmail.com>
2025-02-24 23:07:52 +0900

    feat: develop

--- app/components/Course/CourseDetailsPage.tsx
+++ app/components/Course/CourseDetailsPage.tsx
@@ -2,13 +2,17 @@ import {
   useGetCourseDetailsQuery,
   useGetUsersAllCoursesQuery,
 } from "@/app/redux/features/courses/coursesApi";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Loader from "../Loader/Loader";
 import Heading from "@/app/utils/Heading";
 import Header from "../Header";
 import Footer from "../Footer";
 import CourseDetails from "./CourseDetails";
-
+import {
+  useCreatePaymentIntentMutation,
+  useGetStripePublishablekeyQuery,
+} from "@/app/redux/features/orders/ordersApi";
+import { loadStripe } from "@stripe/stripe-js";
 type Props = {
   id: string;
 };
@@ -17,8 +21,30 @@ const CourseDetailsPage = ({ id }: Props) => {
   const [route, setRoute] = useState("Login");
   const [open, setOpen] = useState(false);
   const { data, isLoading } = useGetCourseDetailsQuery(id);
+  const { data: config } = useGetStripePublishablekeyQuery({});
+  const [createPaymentIntent, { data: paymentIntentData }] =
+    useCreatePaymentIntentMutation();
+  const [stripePromise, setStripePromise] = useState<any>(null);
+  const [clientSecret, setClientSecret] = useState("");
 
-  console.log("Fetched data", data);
+  console.log("Fetched Data", config);
+
+  useEffect(() => {
+    if (config) {
+      const publishablekey = config?.publishablekey;
+      setStripePromise(loadStripe(publishablekey));
+    }
+    if (data) {
+      const amount = Math.round(data.course.price * 100);
+      createPaymentIntent(amount);
+    }
+  }, [config, data]);
+
+  useEffect(() => {
+    if (paymentIntentData) {
+      setClientSecret(paymentIntentData?.client_secret);
+    }
+  }, [paymentIntentData]);
 
   return (
     <>
@@ -38,7 +64,13 @@ const CourseDetailsPage = ({ id }: Props) => {
             setOpen={setOpen}
             activeItem={1}
           />
-          <CourseDetails data={data.course} />
+          {stripePromise && (
+            <CourseDetails
+              data={data.course}
+              stripePromise={stripePromise}
+              clientSecret={clientSecret}
+            />
+          )}
           <Footer />
         </div>
       )}
