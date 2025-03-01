4ab31b60c825dda960c7ff009b3ece702498bc34
Michael777aa <shoxabdufattoyev485@gmail.com>
2025-03-02 00:08:32 +0900

    feat: develop mist

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,8 @@
 
 # PRODUCTION DEPLOYMENT SCRIPT
 
-# Ensure Yarn is installed
-if ! command -v yarn &> /dev/null; then
-  npm install -g yarn
-fi
-
 # Reset any local changes
-# git reset --hard
+git reset --hard
 
 # Switch to master branch
 git checkout master
@@ -16,24 +11,12 @@ git checkout master
 # Pull the latest changes
 git pull origin master
 
-# Install dependencies
-yarn install
+# Install Yarn globally if not installed
 
-# Build the project
-if ! yarn run build; then
-  echo "Build failed!"
-  exit 1
-fi
 
-# Stop & delete previous PM2 process (if exists)
-pm2 stop LMS_FRONTEND || true
-pm2 delete LMS_FRONTEND || true
 
-# Start the application with PM2
-pm2 start "yarn run start" --name=LMS_FRONTEND
+pm2 start "npm run start" --name=frontend
 
-# Save PM2 process list
-pm2 save
 
 # Show logs
-pm2 logs LMS_FRONTEND
+pm2 logs frontend
