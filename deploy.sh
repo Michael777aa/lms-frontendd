87976af572a7e0eb19470ef527e8a3029b0b8e2f
Michael777aa <shoxabdufattoyev485@gmail.com>
2025-03-01 22:45:09 +0900

    feat: evefads

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,34 @@
+#!/bin/bash
 
+# PRODUCTION DEPLOYMENT SCRIPT
 
-# PRODUCTION
+# Reset any local changes
 git reset --hard
+
+# Switch to master branch
 git checkout master
+
+# Pull the latest changes
 git pull origin master
 
+# Install Yarn globally if not installed
+npm install -g yarn
 
-nmp i yarn -g
-yarn global add serve
-yarn 
-yarn run build 
+# Install dependencies
+yarn install
+
+# Build the project
+yarn run build
+
+# Stop & delete previous PM2 process (if exists)
+pm2 stop LMS_FRONTEND || true
+pm2 delete LMS_FRONTEND || true
+
+# Start the application with PM2
 pm2 start "yarn run start" --name=LMS_FRONTEND
+
+# Save PM2 process list
+pm2 save
+
+# Show logs
+pm2 logs LMS_FRONTEND
