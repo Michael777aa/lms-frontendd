42ad967ec5501c26c231c7367cff779827a3e8a0
Michael777aa <shoxabdufattoyev485@gmail.com>
2025-03-01 23:59:37 +0900

    feat:deve

--- app/components/Admin/sidebar/AdminSidebar.tsx
+++ app/components/Admin/sidebar/AdminSidebar.tsx
@@ -2,7 +2,7 @@
 import { FC, JSX, useEffect, useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography } from "@mui/material";
-import "react-pro-sidebar/dist/css/styles.css";
+
 import {
   ArrowForwardIosIcon,
   ArrowBackIosIcon,
