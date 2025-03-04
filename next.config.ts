f6a28ee3ec903738f8304d132f3e47dffa1d0b59
Michael777aa <shoxabdufattoyev485@gmail.com>
2025-03-04 14:49:44 +0900

    faat:

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,15 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true, // ✅ If you're using styled-components
   },
+  async redirects() {
+    return [
+      {
+        source: "/user/me",
+        destination: "http://195.35.9.39:3009/user/me",
+        permanent: false, // Set to true if it's a permanent redirect (301)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
